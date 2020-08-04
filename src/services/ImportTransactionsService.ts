27fd2eb0ce1e2b67bb70fe48130347c876c1a08a
Guilherme Sonnemann Eckert <guilhermeeckert@Guilherme.local>
2020-08-04 00:25:55 -0700

    updated: transactions existes

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,17 +1,30 @@
+import { getCustomRepository, getRepository, In } from 'typeorm';
 import csvParse from 'csv-parse';
 import fs from 'fs';
 import Transaction from '../models/Transaction';
 
+import Category from '../models/Category';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface CSVtransaction {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
+
 class ImportTransactionsService {
   async execute(filePath: string): Promise<Transaction[]> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+    const categoriesRepository = getRepository(Category);
     const contactsReadStream = fs.createReadStream(filePath);
     const parsers = csvParse({
       from_line: 2,
     });
 
     const parseCSV = contactsReadStream.pipe(parsers);
-    const transactions = [];
-    const categories = [];
+    const transactions: CSVtransaction[] = [];
+    const categories: string[] = [];
 
     parseCSV.on('data', async line => {
       const [title, type, value, category] = line.map((cell: string) =>
@@ -26,7 +39,28 @@ class ImportTransactionsService {
     });
 
     await new Promise(resolve => parseCSV.on('end', resolve));
-    return { categories, transactions };
+
+    const existCategories = await categoriesRepository.find({
+      where: {
+        title: In(categories),
+      },
+    });
+
+    const existCategoriesTitle = existCategories.map(
+      (category: Category) => category.title,
+    );
+
+    const addCategoryTitle = categories
+      .filter(category => !existCategoriesTitle.includes(category))
+      .filter((value, index, self) => self.indexOf(value) === index);
+
+    const newCategories = categoriesRepository.create(
+      addCategoryTitle.map(title => ({
+        title,
+      })),
+    );
+
+    await categoriesRepository.save(newCategories);
   }
 }
 
